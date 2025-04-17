dd86d6fe9ceb6141598f7c93cde62cd191e85584
JongTakLim <whdxkr0715@gmail.com>
2025-04-17 23:21:06 +0900

    dd

--- static/js/shedule.js
+++ static/js/shedule.js
@@ -32,8 +32,8 @@ window.onload = function () {
     
         // 1day 일정 (2025년 5월 28일)
         const itemsDay1 = [
-            { start: new Date("2025-05-28 10:00"), end: new Date("2025-05-28 19:00"), id: "item-1" },
-            { start: new Date("2025-05-28 16:00"), end: new Date("2025-05-28 17:00"), id: "item-2" },
+            { start: new Date("2025-04-17 10:00"), end: new Date("2025-04-17 23:59"), id: "item-1" },
+            { start: new Date("2025-04-17 22:00"), end: new Date("2025-04-17 23:59"), id: "item-2" },
             { start: new Date("2025-05-28 18:00"), end: new Date("2025-05-28 19:00"), id: "item-3" },
             { start: new Date("2025-05-28 19:00"), end: new Date("2025-05-28 19:30"), id: "item-4" },
             { start: new Date("2025-05-28 19:30"), end: new Date("2025-05-28 20:30"), id: "item-5" },
@@ -41,7 +41,7 @@ window.onload = function () {
             { start: new Date("2025-05-28 21:00"), end: new Date("2025-05-28 21:30"), id: "item-7" },
             { start: new Date("2025-05-28 21:30"), end: new Date("2025-05-28 22:00"), id: "item-8" }
         ];
-    
+    s
         // 2day 일정 (2025년 5월 29일)
         const itemsDay2 = [
             { start: new Date("2025-05-29 10:00"), end: new Date("2025-05-29 19:00"), id: "item-9" },
@@ -64,11 +64,6 @@ window.onload = function () {
                 if (currentTime >= item.start.getTime() && currentTime <= item.end.getTime()) {
                     itemElement.classList.add("active");
                     itemElement.classList.add("highlight"); // 활성화된 항목에 highlight 클래스 추가
-    
-                    // "진행 중" 텍스트 추가 (중복 방지)
-                    if (!itemText.innerHTML.includes("진행 중")) {
-                        itemText.innerHTML += "<span class='current-event'> 진행 중</span>";
-                    }
                 } else {
                     itemElement.classList.remove("active");
                     itemElement.classList.remove("highlight"); // highlight 클래스 제거
