7cff1b932701039d81094beaefbd83e245712bb8
JongTakLim <whdxkr0715@gmail.com>
2025-04-18 20:12:20 +0900

    dd

--- static/js/home.js
+++ static/js/home.js
@@ -1,24 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // 캐러셀 관련 변수
+    const prevBtn = document.querySelector('.carousel-prev');
+    const nextBtn = document.querySelector('.carousel-next');
     const carouselTrack = document.querySelector('.carousel-track');
-    const items = document.querySelectorAll('.carousel-item');
-    const prevButton = document.querySelector('.carousel-prev');
-    const nextButton = document.querySelector('.carousel-next');
+    const images = [
+        "{{ url_for('static', filename='images/mcyou.jpg') }}", 
+        "{{ url_for('static', filename='images/mcgang.jpg') }}", 
+        "{{ url_for('static', filename='images/mcsin.jpg') }}"
+    ];
     let currentIndex = 0;
-    const itemWidth = items[0].offsetWidth;
-    const totalItems = items.length;
 
+    // 캐러셀 업데이트 함수
     function updateCarousel() {
         carouselTrack.style.transition = 'transform 0.3s ease';
-        carouselTrack.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+        carouselTrack.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
 
-    prevButton.addEventListener('click', function () {
-        currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+    // 이전 이미지
+    prevBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
         updateCarousel();
     });
 
-    nextButton.addEventListener('click', function () {
-        currentIndex = (currentIndex + 1) % totalItems;
+    // 다음 이미지
+    nextBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
         updateCarousel();
     });
 });
