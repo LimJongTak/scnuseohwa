324e2ffb2a8f72e3b377228f719cbafa67068002
JongTakLim <whdxkr0715@gmail.com>
2025-04-21 23:51:02 +0900

    ddddd

--- heroku/app.py
+++ heroku/app.py
@@ -80,6 +80,10 @@ def booth_info(booth_id):
 def notice():
     return render_template("notice.html", page_title="공지사항", show_header_info=False)
 
+@app.route('/test')
+def test():
+    return render_template("test.html", page_title="테스트", show_header_info=False)
+
 # 부스 & 푸드트럭 페이지
 @app.route('/booth-food')
 def booth_food():
