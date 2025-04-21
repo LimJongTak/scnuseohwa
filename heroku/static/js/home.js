1a0b5068fa1a4960c2dba7e4391a4452f7d903c4
JongTakLim <whdxkr0715@gmail.com>
2025-04-21 23:25:36 +0900

    ddddd

--- heroku/static/js/home.js
+++ heroku/static/js/home.js
@@ -1,12 +1,28 @@
 let next = document.querySelector('.carousel-next');
 let prev = document.querySelector('.carousel-prev');
+let track = document.querySelector('.carousel-track');
+let items = document.querySelectorAll('.carousel-item');
 
+// 버튼 클릭 시 슬라이드 전환
 next.addEventListener('click', function() {
-    let items = document.querySelectorAll('.carousel-item');
-    document.querySelector('.carousel-track').appendChild(items[0]);  // 마지막 아이템을 첫 번째로 이동
+    track.appendChild(items[0]);  // 마지막 아이템을 첫 번째로 이동
+    updateActiveClass();
 });
 
 prev.addEventListener('click', function() {
-    let items = document.querySelectorAll('.carousel-item');
-    document.querySelector('.carousel-track').prepend(items[items.length - 1]); // 첫 번째 아이템을 마지막으로 이동
+    track.prepend(items[items.length - 1]);  // 첫 번째 아이템을 마지막으로 이동
+    updateActiveClass();
 });
+
+// 4초마다 자동으로 이미지 슬라이드 전환
+setInterval(function() {
+    track.appendChild(items[0]);  // 마지막 아이템을 첫 번째로 이동
+    updateActiveClass();
+}, 4000);
+
+// 활성화된 클래스 추가 (애니메이션 효과 적용)
+function updateActiveClass() {
+    items = document.querySelectorAll('.carousel-item'); // 슬라이드 아이템 재선택
+    items.forEach(item => item.classList.remove('active')); // 기존 active 클래스 제거
+    items[0].classList.add('active'); // 첫 번째 아이템에 active 클래스 추가
+}
