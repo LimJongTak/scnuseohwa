91a84c4b3953a8edb9452f979849ed7f01de9b21
JongTakLim <whdxkr0715@gmail.com>
2025-05-24 22:45:33 +0900

    fdfd

--- app.py
+++ app.py
@@ -13,217 +13,254 @@ booths = {
         "name": "운영 부스",
         "number": "1",
         "description": "운영 부스입니다.",
-        "details": "추후 안내"
+        "details": "booth1.png",
+        # "image": "a.png"
     },
     "blue_2": {
         "name": "운영 부스",
         "number": "2",
         "description": "운영 부스입니다.",
-        "details": "추후 안내"
+        "details": "booth2.png",
+        # "image": "b.png"
     },
     "blue_3": {
         "name": "또굿집",
         "number": "3",
         "description": "축제준비위원회",
-        "details": "Fairy Tale 향림 대동제의 MD를 현장구매·수령할 수 있는 부스"
+        "details": "booth3.png",
+        # "image": "c.png"
     },
     "blue_4": {
         "name": "과일을 찾아라!",
         "number": "4",
         "description": "총학생회(서화)",
-        "details": "랜덤으로 보여주는 과일·채소 장난감이 일치하면 상품을 받는 확률형 게임형 부스"
+        "details": "booth4.png",
+        # "image": "d.png"
     },
     "blue_5": {
         "name": "슬리퍼 발로 차",
         "number": "5",
         "description": "총동아리연합회(결)",
-        "details": "슬리퍼를 던져 과녁에 넣는 게임을 체험할 수 있는 부스"
+        "details": "booth5.png",
+        # "image": "e.png"
     },
     "blue_6": {
         "name": "탁구공 던지기 게임",
         "number": "6",
         "description": "그린스마트팜스쿨학생회(화랑)",
-        "details": "탁구공을 던져서 맥주잔 안에 넣는 게임을 체험할 수 있는 부스"
+        "details": "booth6.png",
+        # "image": "f.png"
     },
     "blue_7": {
         "name": "소원을 말해봐",
         "number": "7",
         "description": "우주항공첨단소재스쿨학생회(SO:ONE)",
-        "details": "참가자가 동전 던지기 게임을 통해 소원을 적을 수 있는 부스"
+        "details": "booth7.png",
+        # "image": "g.png"
     },
     "blue_8": {
         "name": "2025년 마약류 중독 및 오남용 예방 캠페인",
         "number": "8",
         "description": "약학대학 학생회(도파민)",
-        "details": "약물고글 체험과 올바른 정보 제공을 통해 마약의 위험성을 알리고 안전한 학교 환경 조성을 위한 예방 캠페인을 진행하는 부스"
+        "details": "booth8.png",
+        # "image": "h.png"
     },
     "blue_9": {
         "name": "월하랑 놀자",
         "number": "9",
         "description": "애니메이션문화콘텐츠스쿨",
-        "details": "다채로운 게임과 추첨, SNS 연계 이벤트를 통해 즐거움과 상품을 동시에 제공하는 참여형 부스"
+        "details": "booth9.png",
+        # "image": "i.png"
     },
     "blue_10": {
         "name": "GTEP 사업단",
         "number": "10",
         "description": "GTEP 사업단",
-        "details": "국립순천대학교 GTEP 사업단 홍보활동 및 설명회를 진행하는 부스"
+        "details": "booth10.png",
+        # "image": "j.png"
     },
     "green_11": {
         "name": "양말목 티코스터&음료 리필스테이션",
         "number": "11",
         "description": "중앙 동아리(새순)",
-        "details": "업사이클링 공예와 음료 리필을 통해 탄소중립과 제로웨이스트를 실천하는 환경봉사 동아리 ‘새순’의 체험형 친환경 부스"
+        "details": "booth11.png",
+        # "image": "k.png"
     },
     "green_12": {
         "name": "취향을 담은 꾸러미",
         "number": "12",
         "description": "창업동아리(방향을 잡는 사람들)",
-        "details": "석고 방향제, 팔찌 제작 및 판매를 진행하는 부스"
+        "details": "booth12.png",
+        # "image": "l.png"
     },
     "green_13": {
         "name": "디망쉬",
         "number": "13",
         "description": "디망쉬",
-        "details": "캐릭터소품,굿즈류 판매와 포켓키링,비즈발만들기 체험 진행하는 부스"
+        "details": "booth13.png",
+        # "image": "m.png"
     },
     "green_14": {
         "name": "대학본부 1",
         "number": "14",
         "description": "대학본부 1",
-        "details": "추후 안내"
+        "details": "booth14.png",
+        # "image": "n.png"
     },
     "green_15": {
         "name": "대학본부 2",
         "number": "15",
         "description": "대학본부 2",
-        "details": "추후 안내"
+        "details": "booth15.png",
+        # "image": "o.png"
     },
     "green_16": {
         "name": "대학본부 3",
         "number": "16",
         "description": "대학본부 3",
-        "details": "추후 안내"
+        "details": "booth16.png",
+        # "image": "p.png"
     },
     "green_17": {
         "name": "대학본부 4",
         "number": "17",
         "description": "대학본부 4",
-        "details": "추후 안내"
+        "details": "booth17.png",
+        # "image": "q.png"
     },
     "green_18": {
         "name": "대학본부 5",
         "number": "18",
         "description": "대학본부 5",
-        "details": "추후 안내"
+        "details": "booth18.png",
+        # "image": "r.png"
     },
     "green_19": {
         "name": "대학본부 6",
         "number": "19",
         "description": "대학본부 6",
-        "details": "추후 안내"
+        "details": "booth19.png",
+        # "image": "s.png"
     },
     "green_20": {
         "name": "동화 속 한 장면",
         "number": "20",
         "description": "총학생회(서화)",
-        "details": "인생네컷"
+        "details": "booth20.png",
+        # "image": "t.png"
     },
     "green_21": {
         "name": "동화 속 한 장면",
         "number": "21",
         "description": "총학생회(서화)",
-        "details": "인생네컷"
+        "details": "booth21.png",
+        # "image": "u.png"
     },
     "food_1": {
         "name": "만동만길",
         "number": "1",
         "description": "떡순어묵",
-        "details": "떡볶이 : 6,000원\n순대 : 6,000원\n어묵(5개) : 6,000원\n우동 : 6,000원"
+        "details": "food1.png",
+        # "image": "aa.png"
+        
     },
     "food_2": {
         "name": "VIP",
         "number": "2",
         "description": "크림, 칠리새우",
-        "details": "크림새우/칠리새우\n중 : 10,000원\n대 : 15,000원"
+        "details": "food2.png",
+        # "image": "bb.png"
     },
     "food_3": {
         "name": "키다리푸드",
         "number": "3",
         "description": "야끼소바, 오코노미야끼",
-        "details": "야끼소바 : 10,000원\n오코노미야끼 : 10,000원"
+        "details": "food3.png",
+        # "image": "cc.png"
     },
     "food_4": {
         "name": "부엉이푸드",
         "number": "4",
         "description": "닭꼬치",
-        "details": "닭꼬치 : 5,000원"
+        "details": "food4.png",
+        # "image": "dd.png"
     },
     "food_5": {
         "name": "로드리",
         "number": "5",
         "description": "야채곱창, 무뼈닭발",
-        "details": "야채곱창/무뼈닭발 : 13,000원"
+        "details": "food5.png",
+        # "image": "ee.png"
     },
     "food_6": {
         "name": "뽀라츄러스",
         "number": "6",
         "description": "츄러스, 아이스크림",
-        "details": "츄러스 : 4,000원\n아이스크림 : 4,000원\n아이스크림 츄러스 : 7,000원"
+        "details": "food6.png",
+        # "image": "ff.png"
     },
     "food_7": {
         "name": "이삭",
         "number": "7",
         "description": "생과일주스, 과일화채",
-        "details": "생과일쥬스 : 6,000원\n컵과일 : 7,000원\n과일화채 : 5,000원\n아이스 아메리카노 : 4,000원"
+        "details": "food7.png",
+        # "image": "gg.png"
     },
     "food_8": {
         "name": "알코홀릭",
         "number": "8",
         "description": "주류판매",
-        "details": "추후 안내"
+        "details": "food8.png",
+        # "image": "hh.png"
     },
     "food_9": {
         "name": "톡톡팡팡",
         "number": "9",
         "description": "구슬아이스크림",
-        "details": "구슬아이스크림\n싱글 : 4,000원\n더블 : 7,000\n전체 10,000원"
+        "details": "food9.png",
+        # "image": "ii.png"
     },
     "food_10": {
         "name": "핑크제이",
         "number": "10",
         "description": "소고기불초밥, 연어초밥",
-        "details": "소고기불초밥 : 12,000원\n 연어초밥 : 13,000원"
+        "details": "food10.png",
+        # "image": "jj.png"
     },
     "food_11": {
         "name": "꽃길만걷자6",
         "number": "11",
         "description": "스테이크&덮밥",
-        "details": "스테이크&덮밥 : 12,000원"
+        "details": "food11.png",
+        # "image": "kk.png"
     },
     "food_12": {
         "name": "유에프오",
         "number": "12",
         "description": "뉴욕핫도그, 감자튀김",
-        "details": "뉴욕핫도그(5종) : 5,500원\n감자튀김 : 5,000원\n세트 : 10,000원"
+        "details": "food12.png",
+        # "image": "ll.png"
     },
     "food_13": {
         "name": "꽃길만걷자",
         "number": "13",
         "description": "닭강정",
-        "details": "닭강정\n중 : 10,000원\n대 : 17,000원"
+        "details": "food13.png",
+        # "image": "mm.png"
     },
     "food_14": {
         "name": "명리푸드",
         "number": "14",
         "description": "통닭, 목살바베큐",
-        "details": "통닭바베큐 10,000원\n목살바베큐 18,000원"
+        "details": "food14.png",
+        # "image": "nn.png"
     },
     "food_15": {
         "name": "타임푸드",
         "number": "15",
         "description": "타코야끼",
-        "details": "타코야끼(10알) : 6,000원"
+        "details": "food15.png",
+        # "image": "oo.png"
     }
 }
 # 메인 페이지 (로고 및 날짜 포함)
